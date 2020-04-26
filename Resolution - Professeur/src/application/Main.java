aa66115efe46a216cf752facd2c291feba77425d
mmarc471@gmail.com <mmarc471@gmail.com>
2020-04-26 20:17:18 +0200

    anulation d'erreur stupide

--- Resolution - Professeur/src/application/Main.java
+++ Resolution - Professeur/src/application/Main.java
@@ -6,28 +6,24 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
 	public static Stage stage;
-	//public static AnchorPane pagePrincipale;
-	public static AnchorPane pane;
+	public static AnchorPane pagePrincipale;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//pagePrincipale = FXMLLoader.load(getClass().getResource("MenuPrincipal.fxml"));
-			//Scene scene = new Scene(pagePrincipale, 450, 450);
-			pane = FXMLLoader.load(getClass().getResource("NouvelleExercice.fxml"));
-			Scene scene = new Scene(pane, 910,720);
+			pagePrincipale = FXMLLoader.load(getClass().getResource("MenuPrincipal.fxml"));
+			Scene scene = new Scene(pagePrincipale, 450, 450);
 			primaryStage.setTitle("Résolution V1.0 G8 - Professeur");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			stage = primaryStage;
 			stage.setResizable(false);
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -42,8 +38,8 @@ public class Main extends Application {
 	}
 	
 	public void chargerUnePage(String nomPage) throws IOException {
-		pane = FXMLLoader.load(getClass().getResource(nomPage));
-		//pagePrincipale.getChildren().setAll(pane);
+		AnchorPane pane = FXMLLoader.load(getClass().getResource(nomPage));
+		pagePrincipale.getChildren().setAll(pane);
 	}
 
 	public void setHauteur(int i) {
